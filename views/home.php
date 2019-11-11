0fd294551df12a1d0154ece030b7fbd2bfa4c39a
memyselfandmatthijs <53364717+memyselfandmatthijs@users.noreply.github.com>
2019-11-11 18:44:30 +0100

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,3 +1,30 @@
+<style>
+body{
+    font-family:sans-serif;
+    background:#white;
+    }
+div.titel{
+    background-color:#26a69a;
+}
+h1{ color:white;
+    margin-bottom:0px;
+    padding-left:20px;
+    padding-top:10px;
+
+}
+p{ color:white;
+    margin-top:0px;
+    padding-left:20px;
+    padding-bottom:20px;
+
+}
+
+</style>
+
+<div class="titel">
+<h1>Glitcholotl </h1>
+<p>The social medium for axolotls</p>
+</div>
 <?php
 
 $conn = new mysqli("remotemysql.com", "zwTzrl4wRV", "0t3d9R5kuh", "zwTzrl4wRV");
