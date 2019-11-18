cc0bb677a8209ebcb1257874c3a85218cd8690a2
gllms <willemvanderdoe@hotmail.com>
2019-11-18 12:38:19 +0000

    Use " instead of '

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
 
 // Include router class
-include('Route.php');
+include("Route.php");
 
-// home page
-Route::add('/',function(){
+// Home page
+Route::add("/",function(){
     require "views/home.php";
 });
 
-// Accept only numbers as parameter. Other characters will result in a 404 error
-Route::add('/u/(.*)',function($var1){
+// User page
+Route::add("/u/(.*)",function($var1){
     require "views/user.php";
 });
 
-Route::run('/');
+Route::add("/login",function($var1){
+    require "views/login.php";
+});
+
+Route::run("/");
